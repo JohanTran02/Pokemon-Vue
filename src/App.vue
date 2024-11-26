8675759a670df86c6c097afb359461920b5d16b1
JohanTran <johan.tran0210@gmail.com>
2024-11-26 17:49:45 +0100

    pokemon cards done

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,26 @@
 <script setup lang="ts">
-import { AxiosError } from 'axios';
-import { PokemonClient, type NamedAPIResource, type Pokemon } from 'pokenode-ts';
-import { ref, watchEffect } from 'vue';
+import { ref  } from 'vue';
+import PokemonList from './components/PokemonList.vue';
 import PokemonItem from './components/PokemonItem.vue';
+
 // import { RouterLink, RouterView } from 'vue-router'
 
 const pokemonName = ref("");
 const input = ref("")
-const api = new PokemonClient();
-const pokemon = ref<Pokemon | PokemonAPIList>(<Pokemon | PokemonAPIList>{})
-
-export interface PokemonAPIList {
-  count: number;
-  next: string | null;
-  previous: string | null;
-  results: NamedAPIResource[];
-}
 
 function updatePokemon() {
   pokemonName.value = input.value;
 }
 
-watchEffect(async () => {
-  try {
-    if(pokemonName.value === ""){
-      const response = await api.listPokemons();
-      pokemon.value = response;
-    }else{
-      const response = await api.getPokemonByName(pokemonName.value);
-      pokemon.value = response;
-      console.log(response);
-    }
-  }catch(error){
-    if(error instanceof AxiosError){
-      console.log(error.response);
-    }
-    else throw error;
-  }
-})
 </script>
 
 <template>
-    <div>
+    <main>
       <input type="text" v-model="input">
       <button @click="updatePokemon">Submit</button>
-      <p>{{input}}</p>
-      <ul>
-          <li v-for="pokemonItem in (pokemon as PokemonAPIList).results" :key="(pokemonItem.name)">
-            {{pokemonItem.name}}
-          </li>
-      </ul>
-      <div>
-        <PokemonItem :pokemon="(pokemon as Pokemon)"/>
+      <div class="p-4">
+        <PokemonList v-if="(pokemonName === '')" />
+        <PokemonItem v-else :pokemonName="pokemonName"/>
       </div>
-      <!-- <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav> -->
-    </div>
-
-  <!-- <RouterView /> -->
+    </main>
 </template>
-
-<style scoped>
-
-</style>
--- src/components/PokemonItem.vue
+++ src/components/PokemonItem.vue
@@ -1,16 +1,43 @@
 <script setup lang="ts">
-import { type Pokemon } from 'pokenode-ts';
-import { computed } from 'vue';
+import { AxiosError } from 'axios';
+import { PokemonClient, type Pokemon } from 'pokenode-ts';
+import { computed, ref, watchEffect } from 'vue';
 const props = defineProps<{
-  pokemon: Pokemon
+  pokemonName: string,
 }>()
 
+const api = new PokemonClient();
+const pokemon = ref<Pokemon>({} as Pokemon)
+
 const pokemonImg = computed(() => {
-  if(!props.pokemon.sprites.other?.['official-artwork'].front_default) return "";
-  return props.pokemon.sprites.other?.['official-artwork'].front_default as string ? props.pokemon.sprites.front_default as string : "";
+  const pokemonImage : string = pokemon.value.sprites.other?.['official-artwork'].front_default as string ?? pokemon.value.sprites.front_default as string;
+  return pokemonImage; 
 });
+
+watchEffect(async () => {
+  await getPokemon();
+})
+
+async function getPokemon () {
+  try{
+    pokemon.value = await api.getPokemonByName(props.pokemonName);
+    console.log(pokemon.value)
+  }catch(error){
+    if(error instanceof AxiosError){
+      console.log(error.response);
+    }
+    else throw error;
+  }
+}
 </script>
 
 <template>
-  <p>{{ props.pokemon.name }}</p>
+  <div v-if="pokemon && pokemon.sprites" class="capitalize">
+    <img :src="pokemonImg" :alt="pokemon.name" class="w-full object-cover bg-slate-400">
+    <p>#{{ pokemon.id }}</p>
+    <h1 class="font-bold text-xl py-2">{{ pokemon.name }}</h1>
+    <div class="flex gap-2">
+      <p v-for="types in pokemon.types" :key="types.type.name" class="bg-blue-400 p-1 rounded-md">{{ types.type.name }}</p>
+    </div>
+  </div>
 </template>
