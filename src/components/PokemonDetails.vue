6997261612741eaf33aba44c9df729ccddfb9c9a
JohanTran <johan.tran0210@gmail.com>
2024-11-29 19:32:50 +0100

    moved some pokemon logic to store

--- src/components/PokemonDetails.vue
+++ src/components/PokemonDetails.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
+import { store } from '@/store';
 import { AxiosError } from 'axios';
-import { PokemonClient, type Pokemon, type PokemonSpecies} from 'pokenode-ts';
+import { type Pokemon, type PokemonSpecies, type Type} from 'pokenode-ts';
 import { computed, ref, watchEffect } from 'vue';
 
 const props = defineProps<{
     name : string
 }>();
 
-const api = new PokemonClient();
 const pokemon = ref<Pokemon>({} as Pokemon);
 const pokemonDesc = ref<PokemonSpecies>({} as PokemonSpecies);
+// const pokemonTypes = ref<Type[]>([]);
 const loading = ref<boolean>(false);
 
 interface PokemonStats{
@@ -17,6 +18,26 @@ interface PokemonStats{
     effort: number;
     name: string;
 }  
+
+interface PokemonAbilities{
+    is_hidden: boolean;
+    slot: number;
+    ability : {
+        name: string,
+        url?: string
+    }
+}
+
+// interface PokemonTypes{
+//     weakness: {
+//         double_damage_from:{name: string}[],
+//         half_damage_to: {name: string}[],
+//     },
+//     strength: {
+//         double_damage_to:{name : string}[],
+//         half_damage_from:{name: string}[],
+//     }
+// }
 
 const pokemonImg = computed(() => {
     let pokemonImage: string = "";
@@ -36,24 +57,101 @@ const pokemonStats = computed(() => {
         })
     return stats;
 });
-watchEffect(async () => {
-  await getPokemon();
+
+const pokemonAbiliites = computed(() => {
+    const abilities: PokemonAbilities[] = pokemon.value.abilities.map((ability) => {
+        return {
+            is_hidden: ability.is_hidden,
+            slot: ability.slot,
+            ability: {
+                name: ability.ability.name,
+            }
+        }
+    })
+    return abilities;
 })
 
-async function getPokemon () {
-    loading.value = true;
-  try{
-    pokemon.value = await api.getPokemonByName(props.name);
-    pokemonDesc.value = await api.getPokemonSpeciesByName(props.name);
-    console.log(pokemon.value, pokemonDesc.value)
-  }catch(error){
-    if(error instanceof AxiosError){
-      console.log(error.response);
+// const pokemonTypes = computed(() => {
+//     const types: PokemonTypes = pokemon.value.types.map((type) => {
+//         return {
+//             weakness: {
+//                 double_damage_from : [{name: type.type.name}],
+//                 half_damage_to : [{name: type.type.name}],
+//             },
+//             strength : {
+//                 double_damage_from : [{name: type.type.name}],
+//                 half_damage_to : [{name: type.type.name}]
+//             }
+//         }
+//     })
+//     return types;
+// })
+
+watchEffect(async () => {
+    await PokemonData();
+})
+
+async function PokemonData() {
+    loading.value = true
+    try{
+        const [pokemonValue,pokemonDescValue,pokemonTypesValue] : [Pokemon,PokemonSpecies,Type[]] = await Promise.all([getPokemon(),getPokemonDesc(),getPokemonTypes()]);
+        pokemon.value = pokemonValue;
+        pokemonDesc.value = pokemonDescValue;
+        pokemonTypesValue.push(...pokemonTypesValue);
+        loading.value = false;  
     }
-    else throw error;
-  }finally{
-    loading.value = false
-  }
+    catch(error){
+        if(error instanceof AxiosError){
+        console.log(error.response);
+        }
+        throw error;
+    }
+}
+
+async function getPokemon () : Promise<Pokemon> {
+    try{
+        const response = await store.pokemonEndpoints.getPokemonByName(props.name) ;
+        if(!response) throw new Error("Pokemon data is missing");
+        return response;
+    }catch(error){
+        if(error instanceof AxiosError){
+        console.log(error.response);
+        }
+        throw error;
+    }
+}
+
+async function getPokemonDesc () : Promise<PokemonSpecies> {
+    try{
+        const response = await store.pokemonEndpoints.getPokemonSpeciesByName(props.name);
+        if(!response) throw new Error("Pokemon data is missing");
+        return response;;
+    }catch(error){
+        if(error instanceof AxiosError){
+        console.log(error.response);
+        }
+        throw error;
+    }
+}
+
+async function getPokemonTypes () : Promise<Type[]>{
+    try{
+        const pokemon = await store.pokemonEndpoints.getPokemonByName(props.name);
+
+        const types = pokemon.types.map((type) => {
+            return store.pokemonEndpoints.getTypeByName(type.type.name);
+        })
+        const responses = await Promise.all(types);
+
+        if(!responses) throw new Error("Pokemon data is missing");
+        
+        return responses;
+    }catch(error){
+        if(error instanceof AxiosError){
+        console.log(error.response);
+        }
+        throw error;
+    }
 }
 
 </script>
@@ -63,11 +161,20 @@ async function getPokemon () {
         <div v-if="!loading" class="p-4" >
             <img :src="pokemonImg" :alt="pokemon.name" class="w-full object-cover bg-slate-400">
             <p class="break-keep">{{ pokemonDesc.flavor_text_entries[3].flavor_text }}</p>
-            <p>{{ pokemon.height }}</p>
+            <p>{{ pokemon.height / 10}} m</p>
             <p>{{ pokemon.name }}</p>
-            <p>{{ pokemon.weight }}</p>
+            <p>{{ pokemon.weight / 10}} kg</p>
             <p>{{ pokemon.id }}</p>
-            <p v-for="stats in pokemonStats" :key="stats.name">{{ stats.name }}:{{ stats.base_stat }}</p>
+            <template v-for="abilities in pokemonAbiliites" :key="abilities">
+                <p v-if="!abilities.is_hidden">{{ abilities.ability.name }}</p>
+            </template>
+            <div class="flex gap-2">
+                <p v-for="types in pokemon.types" :key="types.type.name" class="bg-blue-400 p-1 rounded-md">{{ types.type.name }}</p>
+            </div>
+            <div>
+                <h1 class="text-2xl">Stats</h1>
+                <p v-for="stats in pokemonStats" :key="stats.name">{{ stats.name }}:{{ stats.base_stat }}</p>
+            </div>
         </div>
     </div>
 </template>
--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,34 +1,24 @@
 <script setup lang="ts">
 import { AxiosError } from 'axios';
-import { type NamedAPIResource, PokemonClient } from 'pokenode-ts';
-import { ref, watchEffect } from 'vue';
+import { watch } from 'vue';
 import PokemonItem from './PokemonItem.vue';
+import { store } from '@/store';
 
-export interface PokemonAPIList {
-  count: number;
-  next: string | null;
-  previous: string | null;
-  results: NamedAPIResource[];
-}
-
-const api = new PokemonClient();
-const pokemon = ref<PokemonAPIList>(<PokemonAPIList>{})
-
-watchEffect(async () => {
+watch(store.pokemonList, async () => {
   try {
-    pokemon.value = await api.listPokemons(0,12);
+    store.pokemonList = await store.pokemonEndpoints.listPokemons(0,12);
   }catch(error){
     if(error instanceof AxiosError){
       console.log(error.response);
     }
     else throw error;
   }
-})
+}, {immediate:true})
 </script>
 
 <template>
       <ul class="mx-auto">
-          <li v-for="pokemonItem in (pokemon as PokemonAPIList).results" :key="(pokemonItem.name)">
+          <li v-for="pokemonItem in store.pokemonList.results" :key="(pokemonItem.name)">
             <PokemonItem :pokemonName="pokemonItem.name"/>
           </li>
       </ul>
